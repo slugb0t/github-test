1652878f95c2528ac44eb9ee5bc9019fa9b5b8e0
slugb0t <wheresdorian@gmail.com>
2023-01-23 13:32:08 -0800

    test routes

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-
-
-
 module.exports = (app, { getRouter }) => {
   // Your code here
-  
+
   const router = getRouter("/");
 
-  // const instance = axios.create({
-  //   baseURL: "https://probot-slugb0t.vercel.app"
-  // });
+  router.post("/api/github/webhooks", (req, res) => {
+    // Handle webhook event
+    console.log(req.body);
+    res.status(200).end();
+  });
 
-  // router.post("/api/github/webhooks", (req, res) => {
-  //   // Handle webhook event
-  //   console.log(req.body);
-  // });
+  app.route(router);
 
-  // app.route('issues.opened', (req, res) => {
-  //   console.log(req.body);
-  // })
-  
   app.onAny(async (context) => {
     // app.log({event: context.name, action: context.payload[0]})
-    app.log.info({event: context.name, action: context.payload});
-    context.log.info("Connect");
-    // await instance.post('/webhook', context.payload)
+    app.log.info({ event: context.name, action: context.payload });
+    await axios.post(
+      "https://probot-slugb0t.vercel.app/api/github/webhooks",
+      context.payload
+    );
   });
-  
-  app.log.info("Yay, the app was loaded!");
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.log.info("Yay, the app was loaded!");
 };
