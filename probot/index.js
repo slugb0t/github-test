c0a0b353755a65d49874f5d198f0a0daa2d06b41
slugb0t <wheresdorian@gmail.com>
2023-01-24 14:05:14 -0800

    refactor: on any to on installation

--- probot/index.js
+++ probot/index.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     baseURL: "http://localhost:3000",
   });
 
-  app.onAny(async (context) => {
+  app.on("installation", async (context) => {
     // app.log({event: context.name, action: context.payload[0]})
     // app.log.info({ event: context.name, action: context.payload });
     await instance.post("/api/github/webhooks", context.payload);
